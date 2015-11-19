c01e3dcbe58434f4e6fd116523cc35dd846c9f31
Lindani <lindani@projectcodex.co>
2015-11-19 15:21:40 +0200

    Modified admin to display additional fields

--- social_impact_app/social_impact/admin.py
+++ social_impact_app/social_impact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from social_impact.models import  Answers, Complaint
+from social_impact.models import Answers, Complaint
+
 
 class AnswersInline(admin.TabularInline):
     model = Answers
@@ -10,10 +11,12 @@ class AnswersInline(admin.TabularInline):
 class ComplaintAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['complaint_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
     ]
     inlines = [AnswersInline]
     list_filter = ['pub_date']
     search_fields = ['complaint_text']
+    list_display = ('complaint_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Complaint, ComplaintAdmin)
